f15ee4b0e00feefee48b63d3f530352db6e8a1de
SomewhatRobin <jrobinso3@ltu.edu>
2025-11-15 12:15:28 -0500

    Spun Out Update
    Added a script to rotate arms with the player's movement

--- Assets/_Scripts/PlayerControls.cs
+++ Assets/_Scripts/PlayerControls.cs
@@ -7,15 +7,19 @@ public class PlayerControls : MonoBehaviour
     public float rollForce;
     public float speed;
     public Vector3 myWay;
+    public float vecMag = 0f;
+    public Transform bananba;
     // [SerializeField]
     //{ get; private set; }
     private Rigidbody rb;
+    public GameObject hidArms;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         myWay = new Vector3(0, 0, 0);
+       
     }
 
     // Update is called once per frame
@@ -23,36 +27,49 @@ public class PlayerControls : MonoBehaviour
     {
         rollForce = speed;
         Stroll();
+        SpinArms();
+    }
+
+    private void SpinArms()
+    {
+        //There's a castarm in the scene to fix the arms rotating on y instead of z
+        //Fix this so it actually spins arms
+        bananba.localRotation = Quaternion.LookRotation(myWay, Vector3.up);
+        hidArms.transform.localRotation = Quaternion.Euler(-151.6f,0f,bananba.rotation.y);
     }
 
     //Stroll now has movement that accounts for diagonals on Keyboard
-    
+
     private void Stroll()
     {
         float xDir = Input.GetAxis("Horizontal");
         float zDir = Input.GetAxis("Vertical");
-        if ( (xDir*xDir)+(zDir*zDir) > 1.25f) //If the value of the input has it outside a circle w/ radius 1.25 [IF THEY ARE ON KB]
+        vecMag = Mathf.Sqrt((xDir * xDir) + (zDir * zDir)); //Calculates the Magnitude of the vector
+
+        if ((xDir * xDir) + (zDir * zDir) > 1.05f) //If the value of the input has it outside a circle w/ radius 1.05 [IF THEY ARE ON KB]
         {
-            //Sets xDir and zDir to be (sqrt(2) / 2), [so total move speed is 1-ish]
-            //TODO: Make this also work with non-up right directions
+
+            //Sets xDir and zDir to be (Direction / Magnitude), so total move speed is 1.
+            //TODO: Shorten this, it doesn't need the if else anymore
             if (xDir > 0)
             {
-                xDir = 0.7071f;
+                xDir = xDir/vecMag;
             }
             else
             {
-                xDir = -0.7071f;
+                xDir = xDir / vecMag;
             }
 
-            if (zDir > 0) 
+            if (zDir > 0)
             {
-                zDir = 0.7071f;
+                zDir = zDir / vecMag;
             }
             else
             {
-                zDir = -0.7071f;
+                zDir = zDir / vecMag;
             }
         }
+
 
         myWay.x = xDir;
         myWay.z = zDir;
