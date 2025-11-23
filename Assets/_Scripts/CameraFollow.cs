c872b19cae88fcaef1c5de4ee336771098c3854a
SomewhatRobin <jrobinso3@ltu.edu>
2025-11-23 16:58:54 -0500

    Paparazzi Update
    Added a script to get the camera to follow the player
    
    Added animation folder/files for when the player gets hit/when enemies get hit by projectiles

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float yDistance = 17.57f;
+    public float allowableOffset = 3.0f;
+
+    public float xOffset = -4.0f;
+    public float zOffset = -10.0f;
+
+    public Vector3 topLeft;
+    public Vector3 botRight;
+    public GameObject player;
+
+    // Improvements to consider:
+    // - Easing movement at start and end
+    // - Catching up more quickly if player is too far from center
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = player.transform.position + Vector3.right * xOffset + Vector3.up * yDistance + Vector3.forward * zOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position + Vector3.up * yDistance) > allowableOffset)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position 
+                + Vector3.right * xOffset
+                + Vector3.up * yDistance
+                + Vector3.forward * zOffset, speed * Time.deltaTime);
+        }
+
+        Vector3 pos = transform.position;
+
+        if (pos.x < topLeft.x)
+        {
+            pos.x = topLeft.x;
+        }
+
+        if (pos.z > topLeft.z)
+        {
+            pos.z = topLeft.z;
+        }
+
+        if (pos.x > botRight.x)
+        {
+            pos.x = botRight.x;
+        }
+
+        if (pos.z < botRight.z)
+        {
+            pos.z = botRight.z;
+        }
+
+        pos.y = player.transform.position.y + yDistance;
+
+        transform.position = pos;
+
+    }
+}
