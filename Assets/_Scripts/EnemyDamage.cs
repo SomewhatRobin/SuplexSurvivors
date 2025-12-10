e7ee7b1f37af78a6c64a4b9dbe783786d543f4be
CarlotaZafraJimenez <czafrajim@ltu.edu>
2025-12-10 16:57:53 +0100

    EnemyDamage added, changes in UIManager and HP UI

---
+++ Assets/_Scripts/EnemyDamage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int damage = 10;
+    UIManager ui;
+
+    void Start()
+    {
+        ui = FindObjectOfType<UIManager>();
+        if (ui == null) Debug.LogError("[EnemyDamage] No UIManager found in scene!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"[EnemyDamage] OnTriggerEnter. This: {gameObject.name}, Other: {other.name}, Tag: {other.tag}");
+        if (other.CompareTag("Player"))
+        {
+            if (ui == null) Debug.LogError("[EnemyDamage] UIManager null, can't apply damage");
+            else
+            {
+                ui.TakeDamage(damage);
+                Debug.Log("Player hit! -" + damage + " HP");
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"[EnemyDamage] OnCollisionEnter with {collision.gameObject.name} Tag:{collision.gameObject.tag}");
+    }
+}
--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -2,61 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("UI Text")]
     public TextMeshProUGUI ScoreboardTMP;
     public TextMeshProUGUI DebugCmbTMP;
     public TextMeshProUGUI DebugCmb1TMP;
     public TextMeshProUGUI DebugCmb2TMP;
+    public TextMeshProUGUI hpText;
 
-    // Start is called before the first frame update
+    [Header("Health")]
+    public int maxHealth = 50;
+    public int currentHealth;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        UpdateHPUI();
     }
 
-    // Update is called once per frame
     void Update()
     {
         ScoreboardTMP.text = GameManager.Score.ToString();
 
-        //Snake Combo View
-        if (GameManager.combineValue > 0.00001f)
-        {
-            DebugCmbTMP.text = GameManager.combineValue.ToString("#.000000");
-        }
-
-        else 
-        {
-            DebugCmbTMP.text = "Nothing";
-        }
-
-        //Knight Combo view
-        if (GameManager.combineValue1 > 0.00001f)
-        {
-            DebugCmb1TMP.text = GameManager.combineValue1.ToString("#.000000");
-        }
-
-        else
-        {
-            DebugCmb1TMP.text = "Nothing";
-        }
-
-        //Car Combo view
-        if (GameManager.combineValue2 > 0.00001f)
-        {
-            DebugCmb2TMP.text = GameManager.combineValue2.ToString("#.000000");
-        }
-
-        else
-        {
-            DebugCmb2TMP.text = "Nothing";
-        }
-
+        DebugCmbTMP.text = GameManager.combineValue > 0.00001f ? GameManager.combineValue.ToString("#.000000") : "Nothing";
+        DebugCmb1TMP.text = GameManager.combineValue1 > 0.00001f ? GameManager.combineValue1.ToString("#.000000") : "Nothing";
+        DebugCmb2TMP.text = GameManager.combineValue2 > 0.00001f ? GameManager.combineValue2.ToString("#.000000") : "Nothing";
     }
 
-    
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHPUI();
 
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    void UpdateHPUI()
+    {
+        hpText.text = $"{currentHealth}/{maxHealth} HP";
+    }
+
+    void Die()
+    {
+        Debug.Log("Player has died!");
+    }
 }
